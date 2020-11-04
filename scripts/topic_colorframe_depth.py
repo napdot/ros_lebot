ce29d2d0b910351e34b7b6bb111f88238d26e456
luapmatute@gmail.com <luapmatute@gmail.com>
2020-11-04 14:32:58 +0200

    Ball location plus dist in one node

--- scripts/topic_colorframe_depth.py
+++ scripts/topic_colorframe_depth.py
@@ -1,10 +1,91 @@
 import rospy
-from m
+import cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from numpy as np
+from message_filters.msg import TimeSynchronizer
+from additional_msgs.msg import Depth_BallLocation
+import cv2
+import json
+
+
+class Ball:
+    def __init__(self):
+        self.green_parameters = [None]
+        self.ball_location = [None, None, None]
+        self.ball_distance = None
+        self.set_ball_parameters()
+        self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.get_my_image_callback)
+        self.depth_sub = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.get_my_depth_callback)
+        self.ball_depth_location_pub = rospy.Publisher("/ball", Depth_BallLocation)
+        self.ball_depth_location_pub.pub(self.ball_message)
+
+    def get_my_image_callback(self, data):
+        bridge_img = CvBridge()
+        image = bridge_img.compressed_imgmsg_to_cv2(data, "bgr8")
+        self.get_ball_location(image)
+
+    def get_my_depth_callback(self, data):
+        bridge_depth = CvBridge()
+        depth_image = bridge_depth.imgmsg_to_cv2(data, desired_encoding="passthrough")
+        msg_data = Depth_BallLocation
+        ball = [msg_data.x, msg_data.y, msg_data.r]
+        depth_img = msg_data.array  # bitwise mask. Otherwise need to extract pixels and more computation.
+        self.depth_to_ball(depth_img)
+
+    def depth_to_ball(self, depth_img):
+        y1, y2, x1, x2 = (self.ball_location[1] - self.ball_location[2]),\
+                         (self.ball_location[1] + self.ball_location[2]),\
+                         (self.ball_location[0] - self.ball_location[2]),\
+                         (self.ball_location[0] + self.ball_location[2])
+        self.ball_distance = np.mean(depth_img[y1:y2, x1:x2])
+
+    def ball_message(self):
+        msg = Depth_BallLocation()
+        msg.x = self.ball_location[0]
+        msg.y = self.ball_location[1]
+        msg.d = self.ball_distance
+        return msg
+
+    def convert_depth_image(data):
+        bridge = CvBridge()
+        depth_image = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+
+
+    def get_ball_location(self,frame):
+        radius_min = 10
+        radius_max = 50
+        kernel = np.ones((3, 3), np.uint8)
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, tuple(self.green_parameters['min']), tuple(self.green_parameters['max']))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), r) = cv2.minEnclosingCircle(c)
+            if (r < radius_max) & (r > radius_min):
+                cX = x
+                cY = y
+            else:
+                cX = 0
+                cY = 0
+
+            self.ball_location = [cX, cY]
+
+        else:
+            self.ball_location = [0, 0]
+
+    def set_ball_parameters(self):
+        with open('../color_parameters.json') as f:
+            d = json.load(f)
+            self.green_parameters = d['green']
 
 
 
+if __name__ == '__main__':
+    rospy.init_node('/ball_calc', anonymous=False)
+    Ball()
+    rospy.spin()
 
-image_sub = message_filters.Subscriber("image", Image)
-depth_sub = message_filters.Subscriber("depth_image", Image)
-sync = message_filters.TimeSynchronizer([image_sub, depth_sub], 1)
-sync.registerCallback(mask_detect_callback)
