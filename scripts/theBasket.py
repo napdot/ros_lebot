55e2af46f70542c2977426001f89a43b937c5281
luapmatute@gmail.com <luapmatute@gmail.com>
2020-11-05 14:14:55 +0200

    Changes in basket location

--- scripts/theBasket.py
+++ scripts/theBasket.py
@@ -11,8 +11,9 @@ class Basket:
     def __init__(self, color):
         self.red_parameters = None
         self.blue_parameters = None
-        self.basket_location = [0, 0, 0, 0]
+        self.basket_location = [0, 0]
         self.basket_distance = 0
+        self.basket_edges = [0, 0, 0, 0]
         self.set_basket_parameters()
         self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.get_my_image_callback)
         self.depth_sub = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.get_my_depth_callback)
@@ -32,7 +33,7 @@ class Basket:
 
     def depth_to_basket(self, depth_img):
         selected_depth_array = depth_img * self.mask
-        # selected_depth_array = np.mean(depth_img[self.basket[2]:self.basket_location[3],[self.basket[0]:self.basket_location[1]]])
+        # selected_depth_array = np.mean(depth_img[self.basket_edges[3]:self.basket_edges[2], self.basket_edges[1]: self.basket_edges[0]])
         self.basket_distance = np.mean(selected_depth_array)
 
     def basket_message(self):
@@ -48,10 +49,11 @@ class Basket:
             self.mask = cv2.inRange(frame, tuple(self.blue_parameters['min']), tuple(self.blue_parameters['max']))
 
         basket_contours, hierarchy = cv2.findContours(self.mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnt = basket_contours[0]
-        x1, y1, w, h = cv2.boundingRect(cnt)
+        x1, y1, w, h = cv2.boundingRect(basket_contours)
         x2, y2 = x1 + w, y1 + h
-        self.basket_location = [x1, x2, y1, y2]
+        self.basket_edges = [x1, x2, y1, y2]
+        M = cv2.moments(basket_contours)
+        self.basket_location = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
     def set_basket_parameters(self):
         with open('../color_parameters.json') as f:
