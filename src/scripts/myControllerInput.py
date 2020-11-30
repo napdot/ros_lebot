2983c7e0c90fa97c25a8a3fae8c739eb87b1f7d3
Kwasi <kwasiboatjr@live.co.uk>
2020-11-30 13:22:46 +0200

    controllerInput works :) No errors

--- src/scripts/myControllerInput.py
+++ src/scripts/myControllerInput.py
@@ -3,7 +3,6 @@
 import rospy
 from ds4_driver.msg import Report
 from lebot.msg import Wheel
-from math import atan2, cos, sqrt
 
 
 class Cont:
@@ -11,60 +10,44 @@ class Cont:
         self.wheelDistanceFromCenter = 10
         self.wheelAngle = [0, 120, 240]
         self.robotAngularVelocity = 10
-        self.message = Wheel
         self.default_speed = 10
-        self.controller_sub = rospy.Subscriber("/raw_report", Report, self.controller_callback, queue_size=1)
-        self.controller_pub = rospy.Publisher('/wheel_values', Wheel, queue_size=1)
+        self.controller_sub = rospy.Subscriber("/raw_report", Report, self.controller_callback, queue_size=10)
+        self.controller_pub = rospy.Publisher('/wheel_values', Wheel, queue_size=10)
+        self.message = Wheel()
 
     def controller_callback(self, data):
         report = Report()
         report = data
 
-        if report.left_analog_x or report.left_analog_x:
-            self.message.w1, self.message.w2, self.message.w3 = \
-                self.omni(report.left_analog_x, report.left_analog_y)
-        else:
-            if report.dpad_up:
-                self.message.w1 = self.default_speed
-                self.message.w2 = -self.default_speed
-                self.message.w2 = 0
+        self.message.w1 = 0
+        self.message.w2 = 0
+        self.message.w3 = 0
 
-            elif report.dpad_down:
-                self.message.w1 = -self.default_speed
-                self.message.w2 = self.default_speed
-                self.message.w2 = 0
+        if report.dpad_up:
+            self.message.w1 = self.default_speed
+            self.message.w2 = -self.default_speed
+            self.message.w3 = 0
 
-            elif report.dpad_left:
-                self.message.w1 = -self.default_speed
-                self.message.w2 = -self.default_speed
-                self.message.w2 = -self.default_speed
+        elif report.dpad_down:
+            self.message.w1 = -self.default_speed
+            self.message.w2 = self.default_speed
+            self.message.w3 = 0
 
-            elif report.dpad.right:
-                self.message.w1 = self.default_speed
-                self.message.w2 = self.default_speed
-                self.message.w2 = self.default_speed
+        elif report.dpad_left:
+            self.message.w1 = -self.default_speed
+            self.message.w2 = -self.default_speed
+            self.message.w3 = -self.default_speed
+
+        elif report.dpad_right:
+            self.message.w1 = self.default_speed
+            self.message.w2 = self.default_speed
+            self.message.w3 = self.default_speed
 
         self.controller_pub.publish(self.message)
-
-    def omni(self, speed_x, speed_y):
-        x = int(speed_x / 250)
-        y = int(speed_y / 250)
-        try:
-            robotDirectionAngle = atan2(x, y)
-        except:
-            robotDirectionAngle = 0.01
-
-        robotSpeed = sqrt(x * x + y * y)
-
-        wheelLinearVelocity = [0, 0, 0]
-        for i in range(3):
-            wheelLinearVelocity[i] = robotSpeed * cos(robotDirectionAngle - self.wheelAngle[i]) \
-                                     + self.wheelDistanceFromCenter * self.robotAngularVelocity
-
-        return wheelLinearVelocity[0], wheelLinearVelocity[1], wheelLinearVelocity[2]
 
 
 if __name__ == '__main__':
     rospy.init_node('controller_input', anonymous=False)
-    Cont()
+    theC = Cont()
     rospy.spin()
+
