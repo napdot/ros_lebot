c054de3bb0bbfa16ac356489512a66471535aadb
Kwasi <kwasiboatjr@live.co.uk>
2020-12-03 13:14:06 +0200

    Changes

--- src/scripts/tasks/finds_a_ball.py
+++ src/scripts/tasks/finds_a_ball.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import rospy
 import smach
 from lebot.msg import Wheel
-from lebotmovement.findBall import findBall as fball
+from movement.findBall import findBall as fball
 from movement.approachBall import approachBall
 from transfCamCoord import transfCamCoord as tcc
 from calcAngle import calc_angle
@@ -15,13 +15,36 @@ from lebot.msg import Depth_BasketLocation
 
 # ______________________________________________________________________________________________________________________
 
-class FINDBALL(smach.State):
-    def __int__(self):
-        self.msg = Wheel()
-        smach.State.__init__(self, outcomes=['ballFound'])
+class Foo(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['start', 'ballFound', 'atBall'])
+
         self.move = rospy.Publisher('/wheel_values', Wheel, queue_size=1)
-        self.ball_subscriber = rospy.Suscriber('/ball', Depth_BallLocation, self.ball_callback, queue_size=1)
-        self.x, self.y, self.d = 0, 0, 0
+        self.ball_subscriber = rospy.Subscriber('/ball', Depth_BallLocation, self.ball_callback, queue_size=1)
+        self.basket_subscriber = rospy.Publisher('/basket', Depth_BasketLocation, self.basket_callback, queue_size=1)
+
+        self.msg = Wheel()
+        self.msg.w1, self.msg.w2, self.msg3 = 0, 0, 0
+
+    def execute(self):
+        rospy.loginfo('State')
+
+    def ball_callback(self, data):
+        self.ball_x, self.ball_y, self.ball_d = data.x, data.y, data.d
+
+    def basket_callback(self, data):
+        self.basket_x, self.basket_y, self.basket_d = data.x, data.y, data.d
+
+
+class FINDBALL(Foo):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['start', 'ballFound', 'atBall'])
+        self.move = rospy.Publisher('/wheel_values', Wheel, queue_size=1)
+        self.ball_subscriber = rospy.Subscriber('/ball', Depth_BallLocation, self.ball_callback, queue_size=1)
+        self.basket_subscriber = rospy.Publisher('/basket', Depth_BasketLocation, self.basket_callback, queue_size=1)
+
+        self.msg = Wheel()
+        self.msg.w1, self.msg.w2, self.msg3 = 0, 0, 0
 
     def execute(self):
         if (self.x == 0 and self.y == 0) or self.d == 0:
@@ -34,18 +57,8 @@ class FINDBALL(smach.State):
             self.move.publish(self.msg)
             return 'ballFound'
 
-    def ball_callback(self, data):
-        self.x, self.y, self.d = data.x, data.y, data.d
 
-
-class GETTOBALL(smach.State):
-    def __int__(self):
-        self.msg = Wheel()
-        smach.State.__init__(self, outcomes=['atBall'])
-        self.move = rospy.Publisher('/wheel_values', Wheel, queue_size=1)
-        self.ball_subscriber = rospy.Suscriber('/ball', Depth_BallLocation, self.ball_callback, queue_size=1)
-        self.x, self.y, self.d = 0, 0, 0
-
+class GETTOBALL(Foo):
     def execute(self):
         angle = calc_angle(self.x)
         xP, yP = tcc(self.d, angle)
@@ -58,17 +71,18 @@ class GETTOBALL(smach.State):
             self.move.publish(self.msg)
             return 'atBall'
 
-    def ball_callback(self, data):
-        self.x, self.y, self.d = data.x, data.y, data.d
 
-
-class STANDBY(smach.State):
-    def __int__(self):
-        smach.State.__init__(self, outcomes=['start'])
-
+class STANDBY(Foo):
     def execute(self):
         rospy.loginfo('Standby - Now starting....')
         return 'start'
+
+
+class OFF(Foo):
+    def execute(self):
+        rospy.loginfo('OFF - finsihing')
+        rospy.signal_shutdown('find_a_ball done')
+        return 'OFF'
 
 
 # ______________________________________________________________________________________________________________________
@@ -80,14 +94,14 @@ def main():
     sm = smach.StateMachine(outcomes=['OFF', 'pause'])
     with sm:
         smach.StateMachine.add('STANDBY', STANDBY(),
-                               transition={'start': 'FINDBALL'})
+                               transitions={'start': 'FINDBALL', 'ballFound': 'STANDBY', 'atBall': 'STANDBY'})
 
         # Rotate to find ball, once ball is found then go to GETTOBALL
-        smach.StateMachine.add('FINDBALL', FINDBALL(), transition={'ballFound': 'GETTOBALL'})
-
+        smach.StateMachine.add('FINDBALL', FINDBALL(),
+                               transitions={'start': 'FINDBALL', 'ballFound': 'GETTOBALL', 'atBall': 'FINDBALL'})
         # State for approaching ball
         smach.StateMachine.add('GETTOBALL', GETTOBALL(),
-                               transition={'atBall': 'STANDBY'})
+                               transitions={'atBall': 'STANDBY'})
     outcome = sm.execute()
 
 
---
+++ src/scripts/tasks/finds_a_ball_alt.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import rospy
+from lebot.msg import Wheel
+from movement.findBall import findBall as fball
+from movement.approachBall import approachBall
+from transfCamCoord import transfCamCoord as tcc
+from calcAngle import calc_angle
+from lebot.msg import Depth_BallLocation
+from lebot.msg import Depth_BasketLocation
+from lebot.msg import Ref_Command
+
+# ______________________________________________________________________________________________________________________
+
+class Logic():
+    def __init__(self):
+        self.move = rospy.Publisher('/wheel_values', Wheel, queue_size=1)
+        self.ball_subscriber = rospy.Subscriber('/ball', Depth_BallLocation, self.ball_callback, queue_size=1)
+        self.basket_subscriber = rospy.Publisher('/basket', Depth_BasketLocation, self.basket_callback, queue_size=1)
+        self.referee_subscriber = rospy.Subscriber('/referee', Ref_Command, self.referee_callback, queue_size=1)
+
+        self.msg = Wheel()
+        self.msg.w1, self.msg.w2, self.msg3 = 0, 0, 0
+
+        self.current_state = 'Standby'
+        self.last_state = None
+        self.counter = 0
+
+        self.ball_x, self.ball_y, self.ball_d = 0, 0, 0
+        self.basket_x, self.basket_y, self.basket_d = 0, 0, 0
+
+    def execute_state(self):
+        if self.current_state == 'Standby':
+            if self.counter == 0:
+                rospy.loginfo('State: Standby')
+            findBall = self.standby_action()
+            if findBall:
+                self.current_state = 'FindBall'
+                self.counter = 0
+            self.counter = self.counter + 1
+            return
+
+        elif self.current_state == 'FindBall':
+            if self.counter == 0:
+                rospy.loginfo('State: FindBall')
+            getToBall = self.find_ball_action()
+            if getToBall:
+                self.current_state = 'GetToBall'
+                self.counter = 0
+
+            self.counter = self.counter + 1
+            return
+
+
+        elif self.current_state == 'GetToBall':
+            if self.counter == 0:
+                rospy.loginfo('State: GetToBall')
+            atBall = self.get_to_ball_action()
+            if atBall:
+                self.current_state = 'Standby'
+                self.counter = 0
+
+            self.counter = self.counter + 1
+            return
+
+        if self.current_state == 'Pause':
+            return
+
+
+    # Callback from subscriptions
+    def ball_callback(self, data):
+        self.ball_x, self.ball_y, self.ball_d = data.x, data.y, data.d
+
+    def basket_callback(self, data):
+        self.basket_x, self.basket_y, self.basket_d = data.x, data.y, data.d
+
+    def referee_callback(self, data):
+        self.last_state = self.current_state
+        if data.command == 'pause':
+            self.current_state = 'Pause'
+
+        elif data.command == 'resume':
+            self.current_state == self.last_state
+
+    # Actions to perform at each state -------------------
+    def standby_action(self):
+        return True
+
+    def find_ball_action(self):
+        if (self.ball_x == 0 and self.ball_y == 0) or self.ball_d == 0:
+            isBallFound = False
+            self.msg.w1, self.msg.w2, self.msg.w3 = fball(isBallFound)
+            self.move.publish(self.msg)
+            return False
+        else:
+            isBallFound = True
+            self.msg.w1, self.msg.w2, self.msg.w3 = fball(isBallFound)
+            self.move.publish(self.msg)
+            return True
+
+    def get_to_ball_action(self):
+        angle = calc_angle(self.ball_x)
+        xP, yP = tcc(self.ball_d, angle)
+        if self.ball_d > 182.5:
+            self.msg.w1, self.msg.w2, self.msg.w3 = approachBall(xP, yP)
+            self.move.publish(self.msg)
+            return False
+
+        else:
+            self.msg.w1, self.msg.w2, self.msg.w3 = approachBall(xP, yP)
+            self.move.publish(self.msg)
+            return True
+
+    def pause_action(self):
+        pass
+
+# ______________________________________________________________________________________________________________________
+
+if __name__ == '__main__':
+    rospy.init_node('state_machine')
+    IDoLogic = Logic()
+    while not rospy.is_shutdown():
+        IDoLogic.execute_state()
+
