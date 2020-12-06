d6347475041f930d165e5bdb8f19d4bd7a04e0a6
Kwasi <kwasiboatjr@live.co.uk>
2020-12-06 21:40:45 +0200

    I think basket is fine now. Need to check with a basket

---
+++ scripts/myControllerInput.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import rospy
+from ds4_driver.msg import Report
+from lebot.msg import Wheel
+from lebot.msg import Thrower
+
+
+class Cont:
+    def __init__(self):
+        self.wheelDistanceFromCenter = 10
+        self.wheelAngle = [0, 120, 240]
+        self.robotAngularVelocity = 10
+        self.default_speed = 10
+        self.controller_sub = rospy.Subscriber("/raw_report", Report, self.controller_callback, queue_size=10)
+        self.controller_pub = rospy.Publisher('/wheel_values', Wheel, queue_size=10)
+        self.message = Wheel()
+        self.thrower_pub = rospy.Publisher('/thrower_values', Thrower, queue_size=10)
+        self.thrower_message = Thrower()
+        self.thrower_speed = 50
+
+    def controller_callback(self, data):
+        report = Report()
+        report = data
+
+        self.message.w1 = 0
+        self.message.w2 = 0
+        self.message.w3 = 0
+        self.thrower_message.t1 = 0
+
+        if report.dpad_up:
+            self.message.w1 = self.default_speed
+            self.message.w2 = -self.default_speed
+            self.message.w3 = 0
+
+        elif report.dpad_down:
+            self.message.w1 = -self.default_speed
+            self.message.w2 = self.default_speed
+            self.message.w3 = 0
+
+        elif report.dpad_left:
+            self.message.w1 = -self.default_speed
+            self.message.w2 = -self.default_speed
+            self.message.w3 = -self.default_speed
+
+        elif report.dpad_right:
+            self.message.w1 = self.default_speed
+            self.message.w2 = self.default_speed
+            self.message.w3 = self.default_speed
+
+        self.controller_pub.publish(self.message)
+
+        if report.button_cross:
+            self.thrower_message.t1 = self.thrower_speed
+
+        self.thrower_pub.publish(self.thrower_message)
+
+if __name__ == '__main__':
+    rospy.init_node('controller_input', anonymous=False)
+    theC = Cont()
+    rospy.spin()
+
---
+++ scripts/theWheel.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import rospy
+from lebot.msg import Wheel
+import serial
+from lebot.msg import Thrower
+
+
+class Move:
+    def __init__(self):
+        self.ser = serial.Serial("/dev/ttyACM0", timeout=0.03, baudrate=115200,
+                                 bytesize=serial.EIGHTBITS,
+                                 parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+
+        self.wheel_sub = rospy.Subscriber("/wheel_values", Wheel, self.wheel_callback)
+        self.thrower_sub = rospy.Subscriber("/thrower_values", Thrower, self.thrower_callback)
+
+    def move_to(self, w1, w2, w3):
+        sot = ("sd:{0}:{1}:{2}\n".format(w1, w2, w3))
+        self.ser.write(sot.encode('utf-8'))
+        rospy.loginfo(sot)
+
+    def wheel_callback(self, data):
+        self.move_to(data.w1, data.w2, data.w3)
+
+    def throw_at(self, t1):
+        tot = ("d:{0}\n".format(t1))
+        self.ser.write(tot.encode('utf-8'))
+        rospy.loginfo(tot)
+
+    def thrower_callback(self, data):
+        self.throw_at(data.t1)
+
+
+if __name__ == '__main__':
+    rospy.init_node('vruum', anonymous=False)
+    IWhoMoves = Move()
+    rospy.spin()
