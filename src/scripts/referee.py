6e4f876f0e3bdfc3657029b018105202504d5d46
luapmatute@gmail.com <luapmatute@gmail.com>
2020-12-14 20:36:57 +0200

    Htlm done, need to update referee node

--- src/scripts/referee.py
+++ src/scripts/referee.py
@@ -1,30 +1,26 @@
 #!/usr/bin/env python3
+
 import rospy
 from rospy_websocker_client import WebsocketROSClient as ros_ws
 from st_msgs.msg import String
+from lebot.msg import Ref_Command
+
 
 # https://github.com/GigaFlopsis/rospy_websocker_client
 
 class Signal:
     def __init__(self):
         self.ws_client.Subscriber('/ref_signal', String(), signal_callback())
-        self.ref_signals.Publisher('/referee', String(), signal_callback())
+        self.ref_signals.Publisher('/referee', Ref_Command()
 
     def signal_callback(self, data):
+        self.ws
 
-
-
-
-
-def main():
-    rospy.init_node('/ref')
-    Signal()
-    rospy.spin()
 
 
 if __name__ == '__main__':
-    main()
-
-
+    rospy.init_node('ref', anonymous=False)
+    Signal()
+    rospy.spin()
 
 
