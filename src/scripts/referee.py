ab0c9179ef2992394f4e75c396b70fa6a19fde82
luapmatute@gmail.com <luapmatute@gmail.com>
2020-12-15 09:22:38 +0200

    Some progress

--- src/scripts/referee.py
+++ src/scripts/referee.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
 import rospy
-from rospy_websocker_client import WebsocketROSClient as ros_ws
 from st_msgs.msg import String
 from lebot.msg import Ref_Command
 
-
-# https://github.com/GigaFlopsis/rospy_websocker_client
-
 class Signal:
-    def __init__(self):
-        self.ws_client.Subscriber('/ref_signal', String(), signal_callback())
-        self.ref_signals.Publisher('/referee', Ref_Command()
+    def __init__(self, topic):
+        self.sub_topic = topic
+        self.ws_client.Subscriber(self.sub_topic, String(), self.signal_callback())
+        self.ref_signals.Publisher('/referee', Ref_Command())
 
-    def signal_callback(self, data):
-        self.ws
+    def signal_callback(self, str):
+        string = str.data
+        st
+
 
 
 
 if __name__ == '__main__':
     rospy.init_node('ref', anonymous=False)
-    Signal()
+    topic = '/js_ref'
+    Signal(topic)
     rospy.spin()
 
 
