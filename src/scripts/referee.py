15d4be5232bc51465f14505fd2b7b3368c8186d5
luapmatute@gmail.com <luapmatute@gmail.com>
2020-12-17 13:06:40 +0200

    Hopefully a functional Client connection

--- src/scripts/referee.py
+++ src/scripts/referee.py
@@ -1,61 +1,35 @@
 #!/usr/bin/env python3
 
 import rospy
-from st_msgs.msg import String
+from std_msgs.msg import String
 from lebot.msg import Ref_Command
 import json
-import websocket as ws
-import thread
-import asyncio
-
 
 
 class Signal:
     def __init__(self):
-        self.ref_signals.Publisher('/referee', Ref_Command(), queue_size=5)
+        self.ws_client.Subscriber('/js_ref', String(), self.signal_callback, queue_size=10)
+        self.ref_signals.Publisher('/referee', Ref_Command(), queue_size=10)
 
-    def analyze_command(self, str):
-        ref_obj = json.loads(str)
+    def signal_callback(self, data):
+        ref_obj = json.loads(data)
         targets = ref_obj['targets']
         try:    # if target.index returns error, means that we are not on target list and don't need to change anything.
             index = targets.index('LeBot')
+            cm = Ref_Command()
             if ref_obj['signal'] == 'start':
-                cm = Ref_Command
                 cm.command = 'resume'
                 self.ref_signals.publish(cm)
-                color = refObj['targets'][index]
+                color = ref_obj['targets'][index]
                 rospy.set_param("basket_color", color)
             elif ref_obj['signal'] == 'stop':
-                cm = Ref_Command
                 cm.command = 'pause'
                 self.ref_signals.publish(cm)
-            return
         except:
-            return
-
-
-async def execute():
-    global msg_ws, new_msg
-    ws_addr = 'ws://127.0.0.1:9000'
-    new_msg = False
-    async with websockets.connect(ws_addr) as websocket:
-        while True:
-            msg_ws = await websocket.recv()
-            if not new_msg:
-                new_msg = True
-
-
-def init_ws():
-    asyncio.get_event_loop().run_until_complete(execute())
+            pass
 
 
 if __name__ == '__main__':
-    new_msg = False
     rospy.init_node('ref', anonymous=False)
-    ref = Signal()
-    t = Thread(target=init_ws, daemon=True)
-    t.start
-    while not rospy.is_shutdown():
-        if new_msg:
-            ref.analyze_command(msg_ws)
-            new_msg = False
+    Signal()
+    rospy.spin()
