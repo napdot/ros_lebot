af203a4c4c2c30082d3a3699f89f7fffb1821ad8
luapmatute@gmail.com <luapmatute@gmail.com>
2020-12-20 20:45:43 +0200

    Fixed mistake

--- src/scripts/calcAngle.py
+++ src/scripts/calcAngle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-def calc_angle(x_coord):
-    cameraWidth = 640
-    hfov = 69.4
-    return (x_coord - (cameraWidth / 2) / (cameraWidth / 2)) * (hfov / 2)
+import numpy as np
+
+def calc_angle(x_coord, adj):
+    angle = np.arcsin((x_coord / adj))
+    return angle
