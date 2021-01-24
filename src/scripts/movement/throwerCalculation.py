af07e2566b7485522cc37b83a1d06b2bbfdc2e60
napdot <luapmatute@gmail.com>
2021-01-24 12:16:14 +0200

    Testing all

--- src/scripts/movement/throwerCalculation.py
+++ src/scripts/movement/throwerCalculation.py
@@ -1,9 +1,9 @@
 
 def thrower_calculation(basket_distance):
-    trange = [1390, 2000]
-    ser = int(basket_distance * 0.208045 + 1117.105967)
+    trange = [0, 100]
+    ser = int(basket_distance * 0.01352 + 18.79578)
     if ser < trange[0]:
-        ser = 1390
+        ser = int(23)
     if ser > trange[1]:
-        ser = 2000
+        ser = int(99)
     return ser
